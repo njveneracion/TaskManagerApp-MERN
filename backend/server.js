5fdd42e54ce2181e1412d64089406ad862dab846
NJ Veneracion <enjayveneracion@gmail.com>
2024-12-01 03:07:07 +0800

    taskapp ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -7,31 +7,19 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
 import taskRoutes from "./routes/task.route.js";
 dotenv.config();
-import path from "path";
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Adjust this to your frontend URL
+    origin: "http://localhost:5137", // Adjust this to your frontend URL
     methods: ["GET", "POST"],
   },
 });
 
 app.use(cors());
 app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
